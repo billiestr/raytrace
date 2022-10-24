c14718baaa89d9ac4422e3e64383ca0cd78cc90d
billiestr <billieagcave@gmail.com>
2022-10-24 11:25:53 +0100

    updated window messafe

--- initial.py
+++ initial.py
@@ -4,4 +4,4 @@ from pygame.locals import QUIT
 pygame.init()
 SIZE=(1200, 400)
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption('raycast - go backwards to clip through barriers!')
+pygame.display.set_caption('raycast - bump into something to change house colour!')
