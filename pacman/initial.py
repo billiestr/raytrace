8c14cce7c45731f6485698b1a28cb27783e8f40b
billiestr <billieagcave@gmail.com>
2022-10-24 15:14:30 +0100

    changed window message

--- pacman/initial.py
+++ pacman/initial.py
@@ -4,4 +4,4 @@ from pygame.locals import QUIT
 pygame.init()
 SIZE=(1200, 400)
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption('raycast - bump into something to change house colour!')
+pygame.display.set_caption('PACMAN! - teleport at portals!!')
