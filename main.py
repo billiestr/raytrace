e9d650c74a672bb7f6da633b7df96453baaa1fba
billiestr <billieagcave@gmail.com>
2022-10-24 11:26:40 +0100

    fixed movement order and added colour selection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import M
 from initial import screen, SIZE
 width, height = SIZE[0], SIZE[1]
 
@@ -20,6 +21,17 @@ pink, brown = (255, 50, 50), (139, 64, 0)
 #line
 b1 = Boundary((50, 160), (165, 186))
 b1.colour = g
+
+#colour selection
+selection = []
+start, cwidth, gap = 30, 10, 15
+colours = [r, g, b, y, c, m]
+for i, colour in enumerate(colours):
+	pstart = start + (i * gap)
+	picker = Boundary((pstart, 379), (pstart+cwidth, 379))
+	picker.colour = colour
+	selection.append(picker)
+
 #quadrilateral
 vertices = [(20, 30), (70, 30), (70, 80), (20, 80)]
 square = BoundaryPolygon(vertices, True)
@@ -51,10 +63,11 @@ bound.set_colour(white)
 
 clock = pygame.time.Clock()
 ROTVELOCITY = .1
-MOVEVELOCITY = .05
-walls = square.boundarys+triangle.boundarys+bound.boundarys+house+[b1]
+MOVEVELOCITY = .04
+walls = square.boundarys+triangle.boundarys+bound.boundarys+house+[b1]+selection
 done = False
 print('drawing')
+bgcolour = black
 while not done:
 	tm = clock.tick(60)
 	movev = MOVEVELOCITY * tm
@@ -63,14 +76,14 @@ while not done:
 		if event.type == QUIT:
 			done = True
 	keys = pygame.key.get_pressed()
-	if keys[K_LEFT]:
-		p1.set_rot(p1.rot-rotv)
-	if keys[K_RIGHT]:
-		p1.set_rot(p1.rot+rotv)
 	if keys[K_UP]:
 		p1.move(movev, walls)
 	if keys[K_DOWN]:
 			p1.move(-movev, walls)
+	if keys[K_LEFT]:
+		p1.set_rot(p1.rot-rotv)
+	if keys[K_RIGHT]:
+		p1.set_rot(p1.rot+rotv)
 	if keys[K_i] and p1.vd>1:
 			p1.vd -= 1
 	if keys[K_o]:
@@ -81,9 +94,16 @@ while not done:
 	
 	p1.update_points(walls, False)
 	r = Render(p1.get_points(), p1.vd)
+	for colour in (v.colour for v in selection):
+		if p1.check_interaction(colour):
+			print(True)
+			#done = True
+			housewalls.set_colour(colour)
+			break
 
+	
 	#draw code
-	screen.fill(black)
+	screen.fill(bgcolour)
 	r.show()
 	b1.show()
 	square.show()
@@ -91,6 +111,9 @@ while not done:
 	#house
 	housewalls.show()
 	wardrobe.show()
+	#selection
+	for picker in selection:
+		picker.show()
 	#raycaster
 	p1.show(y)
 	p1.show_ray(p1.rays[len(p1.rays)//2], c, 2)
